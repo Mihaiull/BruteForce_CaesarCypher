e2a9b860082c72d0bfd707997481d5b545b3d721
Mihaiull <116212492+Mihaiull@users.noreply.github.com>
2025-03-09 22:20:37 +0200

    clean up the repository

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <math.h>
+#include "logic.h"
+
+using namespace std;
+
+
+int main()
+{
+	UI();
+	return 0;
+}
